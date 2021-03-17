ef3574e5326937b68213fc3f5438ddf9fce19d93
hongkai-bu <315578091@qq.com>
2021-03-18 00:28:27 +0800

    hahaha

--- study/src/main/java/com/hongkai/study/controller/HelloController.java
+++ study/src/main/java/com/hongkai/study/controller/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String sayHello(){
-        return "Hello Spring Boot!";
+        return "Hello Spring Boot! hahahahahahhah";
     }
 }
